468711bb5d5da0d35a891a88ff4987b85b375135
asha <ashaagm@gmail.com>
2018-03-23 12:01:54 -0400

    adding heroku env

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,17 @@ var mysql = require("mysql");
 
 var connection;
 
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+
 connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
 	password: "",
 	database: "burgers_db"
 });
-
+}
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -19,3 +23,4 @@ connection.connect(function(err) {
 });
 
 module.exports = connection;
+
